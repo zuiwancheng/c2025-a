1ee175d7fd681f3e342e64bf54927ab55519f4c6
Kun_9 <82296366+zuiwancheng@users.noreply.github.com>
2025-09-07 16:55:39 +0800

    Update main.c

--- level1/p02_is_prime/main.c
+++ level1/p02_is_prime/main.c
@@ -1,6 +1,21 @@
-#include <printf.h>
+#include <iostream>
+#include <cmath>
 
-int main() {
-    printf("hello world!\n");
+int main( ) {
+    int n;
+    std::cin >> n;
+    if (!(n & 1)) {
+        printf("%d is not a prime!\n",n);
+        return 0;
+    }
+    int ub = sqrt(n);
+    for (int i = 2; i <= ub; i++) {
+        if (n % i == 0) {
+            printf("%d is not a prime!\n",n);
+            return 0;
+        }
+    }
+    printf("%d is a prime!\n",n);
+
     return 0;
 }
