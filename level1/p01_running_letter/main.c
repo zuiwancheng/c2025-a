a919359b8a7116bb41cf279cb0a34a8727c3bd00
Kun_9 <82296366+zuiwancheng@users.noreply.github.com>
2025-09-07 15:57:41 +0800

    Update main.c

--- level1/p01_running_letter/main.c
+++ level1/p01_running_letter/main.c
@@ -1,6 +1,21 @@
-#include <printf.h>
+#include <iostream>
 
-int main() {
-    printf("hello world!\n");
+int main( ) {
+    while (1) {
+        for (int i = 1; i <= 127; i++) {
+            for (int j = 0; j < i; j++){
+                putchar(' ');
+            }
+            putchar('a');
+            system("cls");
+        }
+        for (int i = 1; i <= 127; i++) {
+            for (int j = 0; j < 127 - i; j++) {
+                putchar(' ');
+            }
+            putchar('a');
+            system("cls");
+        }
+    }
     return 0;
 }
