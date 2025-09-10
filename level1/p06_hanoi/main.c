8ca4c5a137d8eeaa694be5153997b1883f8584bc
Kun_9 <82296366+zuiwancheng@users.noreply.github.com>
2025-09-10 11:56:02 +0800

    submit for hanoi

--- level1/p06_hanoi/main.c
+++ level1/p06_hanoi/main.c
@@ -1,6 +1,30 @@
-#include <printf.h>
+#include <iostream>
 
-int main() {
-    printf("hello world!\n");
+using namespace std;
+
+void out(int bg, int ed) {
+    cout << (bg == 1 ? 'a' : (bg == 2 ? 'b' : 'c'));
+    cout << " -> ";
+    cout << (ed == 1 ? 'a' : (ed == 2 ? 'b' : 'c'));
+    cout << endl;
+}
+
+void move(int n, int bg, int ed, int tmp) {
+    if (n == 1) {
+        out(bg, ed);
+        return;
+    }
+    move(n - 1, bg, tmp, ed);
+    out(bg, ed);
+    move(n - 1, tmp, ed, bg);
+}
+
+int main( ) {
+
+    int n = 0;//n 层汉诺塔
+    cin >> n;
+
+    move(n , 1, 2, 3);
+
     return 0;
 }
